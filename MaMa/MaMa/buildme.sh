0418ddc496db95aa294abe10a022eafafbe70d2d
Vedran Novakovic <venovako@venovako.eu>
2019-05-13 12:40:28 +0200

    Prepare the builds for macOS and Linux.

--- MaMa/MaMa/buildme.sh
+++ MaMa/MaMa/buildme.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-g++ -O3 -march=native -DPLATFORM_x64_GNU *.cpp -o ../../bin/MaMa
+if [ `uname` = "Darwin" ]
+then
+	echo "Building MaMa.osx"
+	clang++ -O3 -march=native -integrated-as -DPLATFORM_x64_GNU *.cpp -o ../../bin/MaMa.osx
+	strip ../../bin/MaMa.osx
+else
+	echo "Building MaMa.lnx"
+	g++ -O3 -march=native -static -DPLATFORM_x64_GNU *.cpp -o ../../bin/MaMa.lnx
+	strip --strip-unneeded ../../bin/MaMa.lnx
+fi
--- MaMa/MaMaC/buildme.sh
+++ MaMa/MaMaC/buildme.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-g++ -O3 -march=native -DPLATFORM_x64_GNU *.cpp -o ../../bin/MaMaC
+if [ `uname` = "Darwin" ]
+then
+	echo "Building MaMaC.osx"
+	clang++ -O3 -march=native -integrated-as -DPLATFORM_x64_GNU *.cpp -o ../../bin/MaMaC.osx
+	strip ../../bin/MaMaC.osx
+else
+	echo "Building MaMaC.lnx"
+	g++ -O3 -march=native -static -DPLATFORM_x64_GNU *.cpp -o ../../bin/MaMaC.lnx
+	strip --strip-unneeded ../../bin/MaMaC.lnx
+fi
