61ef190c0eb093563831f0aa67416bba5fd05220
Vedran Novakovic <venovako@venovako.eu>
2023-11-09 00:25:14 +0100

    Rebuild the executables.

--- MaMa/MaMaC/buildme.sh
+++ MaMa/MaMaC/buildme.sh
@@ -2,14 +2,14 @@
 if [ `uname` = "Darwin" ]
 then
 	echo "Building MaMaC.osx"
-	clang++ -O3 -DPLATFORM_x64_GNU *.cpp -o MaMaC.x64 -target x86_64-apple-macos11
-	clang++ -O3 -DPLATFORM_x64_GNU *.cpp -o MaMaC.arm -target arm64-apple-macos11
+	clang++ -Ofast -DPLATFORM_x64_GNU *.cpp -o MaMaC.x64 -target x86_64-apple-macos11
+	clang++ -Ofast -DPLATFORM_x64_GNU *.cpp -o MaMaC.arm -target arm64-apple-macos11
 	lipo -create -output MaMaC.osx MaMaC.x64 MaMaC.arm
 	rm -f MaMaC.x64 MaMaC.arm
 	mv -f MaMaC.osx ../../bin
 	lipo -detailed_info ../../bin/MaMaC.osx
 else
 	echo "Building MaMaC.lnx"
-	g++ -O3 -static -DPLATFORM_x64_GNU *.cpp -o ../../bin/MaMaC.lnx
+	g++ -Ofast -static -DPLATFORM_x64_GNU *.cpp -o ../../bin/MaMaC.lnx
 	strip --strip-unneeded ../../bin/MaMaC.lnx
 fi
